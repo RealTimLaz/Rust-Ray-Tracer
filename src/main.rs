b3f9bfa2f340e9185cbdd4ef9607156dc5afe699
Tim Lazarus <me@timlazarus.com>
2021-02-25 23:50:02 +0000

    Add ppm generator

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
+
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("{}", 255);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g = j as f64 / (IMAGE_WIDTH - 1) as f64;
+            let b = 0.25;
+
+            let ir = (r * 255.0) as u8;
+            let ig = (g * 255.0) as u8;
+            let ib = (b * 255.0) as u8;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}
