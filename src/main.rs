cc47e7588c402831d71bb580a08925dfc7a81202
Tim Lazarus <me@timlazarus.com>
2021-03-01 20:22:40 +0000

    Add function to write a simple PPM

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+fn write_ppm() {
+    let image_width = 256;
+    let image_height = 256;
+
+    print!("P3\n{} {}\n255\n", image_width, image_height);
+
+    for j in (0..image_height).rev() {
+        for i in 0..image_width {
+            let r = (i as f64) / (image_width - 1) as f64;
+            let g = (j as f64) / (image_height - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}
+
 fn main() {
-   println!("Hello, world!");
+    write_ppm();
 }
